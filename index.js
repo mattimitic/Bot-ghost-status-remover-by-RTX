10137d1ab3fecef1a1c466d846944f08dfa888a6
mattimitic <165468375+mattimitic@users.noreply.github.com>
2024-03-30 10:21:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["RACEDAY COMMUNITY","GIOCA ORA","DAILY RACES"];
 
 
 let currentIndex = 0;
